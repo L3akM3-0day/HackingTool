b1ba5479339f512f64d5d5c03277c1f0d9a979f6
L3akM3-0day <L3akM3-0Day@protonmail.com>
2016-10-18 23:35:41 +0000

    spiderPath updated

--- SpiderPath/spiderPath.py
+++ SpiderPath/spiderPath.py
@@ -4,7 +4,32 @@ import urllib.request
 
 url = input('URL TO SPIDER : ')
 print(url)
-try:
+try:		
 	html = urllib.request.urlopen(url)
-except:
-	print("can't open url")	
+except ValueError:
+	print("Can't open url {}".format(url))
+soup = BeautifulSoup(html,"html5lib")
+print('BEGIN SPIDERING')
+Followed = []
+def getLink(soup):
+	global Followed
+	links = []
+	for a in soup.find_all('a', href=True):
+		try:
+			if a['href'] not in Followed:
+				print('URL FOUND : {}'.format(a['href']))	
+				Followed.append(a['href'])
+				if a['href'].startswith('/') or a['href'].startswith('?'):
+					links.append(a['href'])
+		except UnicodeDecodeError:
+			pass
+	for link in links:
+		try:
+			html = urllib.request.urlopen(url + link)
+			Followed.append(link)
+			soup = BeautifulSoup(html,"html5lib")
+			getLink(soup)
+		except:
+			pass
+
+getLink(soup)
